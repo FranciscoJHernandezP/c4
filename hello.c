7e90f894c8be23756c5194a04b86c85182d1c78d
Francisco Hernández <francisco.hernandez@tec.mx>
2021-03-18 15:00:13 +0000

    Incluimos primer programa en C

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*Programa sencillo en C*/
+/*La salida debe ser Hello World en la terminal*/
+
+int main(int argc, char **argv)
+{
+	printf("Hello World\n");
+	return 0;
+}
+
+
